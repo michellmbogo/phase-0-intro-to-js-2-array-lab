e8f7e60e987f177de584cc7ff9791e892da7fcd4
Michell Wambui Mbogo <mishie254@gmail.com>
2024-12-08 12:28:35 +0600

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+
+// Initialize the cats array
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Non-destructive functions
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+// Destructive functions
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
